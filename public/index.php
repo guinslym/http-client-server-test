cd723f98f23eae34912160317d72b597ab5093be
Félix Dubé <felix.dube404@gmail.com>
2016-04-15 09:25:09 -0400

    Made the display of the http response work.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<pre>
+<?php
 
    # TIP: Use the $_SERVER Sugerglobal to get all the data your need from the Client's HTTP Request.
 
@@ -30,22 +31,12 @@
 
     #Body creation
 
-    $id = "\t".'"@id":'.$httpRequest->getRequestTarget().'",';
-    $timeSent = "\t".'"timeSent":"'.time().'"';
+    $bodyArray = array('@id'=> $httpRequest->getRequestTarget(), 'to' => 'Pillr', 'subject'=>'Hello Pillr','message'=>'Here is my submission','from'=>'Felix Dube','timesent'=>time());
+    $bodyJson = json_encode($bodyArray,JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
 
     $body = new Stream;
-    $body->write('{');
-    $body->write($id);
-    $body->write("\t".'"to": "Pillr",');
-    $body->write("\t".'"subject": "Hello Pillr",');
-    $body->write("\t".'"message": "Here is my submission.",');
-    $body->write("\t".'"from": "Felix Dube",');
-    $body->write($timeSent);
-    $body->write("}");
-
+    $body->write($bodyJson);
     $httpResponse = $httpResponse->withBody($body);
-
-
 
     #Headers
 
@@ -55,16 +46,17 @@
     $httpResponse = $httpResponse->withAddedHeader('Content-Length',strlen($httpResponse->getBody()->__toString()));
     $httpResponse = $httpResponse->withAddedHeader('Content_Type','application/json');
 
-    echo nl2br("HTTP/".$httpResponse->getProtocolVersion().' '.$httpResponse->getStatusCode().' '.$httpResponse->getReasonPhrase().PHP_EOL);
+    echo "HTTP/".$httpResponse->getProtocolVersion().' '.$httpResponse->getStatusCode().' '.$httpResponse->getReasonPhrase().PHP_EOL;
     foreach($httpResponse->headers as $name => $value)
     {
-        echo nl2br($name.': '.$value.PHP_EOL);
+        echo $name.': '.$value.PHP_EOL;
     }
 
-    echo nl2br($httpResponse->getBody()->__toString());
+    echo $httpResponse->getBody()->__toString();
 
 
 
  ?>
+</pre>
  </body>
 </html>
